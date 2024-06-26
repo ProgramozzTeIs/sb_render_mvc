7ad335c9db58855712665087ba11061a4e4393c4
Bajsz <bajortamas@hotmail.com>
2024-06-26 15:12:50 +0200

    Add Dockerfile and Rest API

---
+++ src/main/java/pti/sb_render_mvc/controller/AppController.java
@@ -0,0 +1,14 @@
+package pti.sb_render_mvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+	@GetMapping("/")
+	public String index() {
+		
+		return "Hello Render!";
+	}
+}
