91097cd145cdf3f60b8d936f80ddedfa95110e79
Bajsz <bajortamas@hotmail.com>
2024-06-26 15:08:18 +0200

    Init project

---
+++ src/main/java/pti/sb_render_mvc/SbRenderMvcApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_render_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRenderMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRenderMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pti/sb_render_mvc/SbRenderMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_render_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRenderMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
